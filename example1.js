ddaedca67c1e97c1f3ec214c3d0f835283b9b5ca
Elina <elinatymtsev@gmail.com>
2021-10-24 12:58:10 +0300

    11lesson

---
+++ example1.js
@@ -0,0 +1,4 @@
+let a = prompt ("Please enter the side length of the square");
+let b = 4*a;
+
+alert('Perimeter of your square = ' + b);
---
+++ example2.js
@@ -0,0 +1,10 @@
+let a = prompt ("Please enter a number");
+
+if (a % 2)  {
+    (a * 9)
+    alert ('Your number is odd and multiplied by 9 = ' + a*9)
+
+ } else if(a / 2) {
+    (a * 8)
+    alert ('Your number is even and multiplied by 8 = ' + a*8)
+ }
